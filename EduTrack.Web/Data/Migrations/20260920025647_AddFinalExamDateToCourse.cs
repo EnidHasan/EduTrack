ccea4d6ee8ea26ba49929a8a7948be9361f04dc9
EnidHasan <enidhasan@users.noreply.github.com>
2026-09-20 09:24:04 +0600

    feat(course): add FinalExamDate and FinalExamTime fields with EF migration

---
+++ EduTrack.Web/Data/Migrations/20260920025647_AddFinalExamDateToCourse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduTrack.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFinalExamDateToCourse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FinalExamDate",
+                table: "Courses",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FinalExamTime",
+                table: "Courses",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FinalExamDate",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "FinalExamTime",
+                table: "Courses");
+        }
+    }
+}
